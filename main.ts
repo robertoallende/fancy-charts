ac6291d31ac3119e35f31e27a18892a8463052ed
Roberto Allende <rallende@gmail.com>
2026-08-02 21:15:48 +1200

    Complete Unit 03: wire postprocessor in main.ts
    registerPostprocessor() called from onload(). Plugin now renders
    fancy-charts code blocks end-to-end.

--- main.ts
+++ main.ts
@@ -1,19 +1,10 @@
-import { Notice, Plugin } from "obsidian";
+import { Plugin } from "obsidian";
+import { registerPostprocessor } from "./src/integration/postprocessor";
 
 export default class FancyChartsPlugin extends Plugin {
   async onload() {
-    new Notice("Fancy Charts loaded");
-
-    this.addCommand({
-      id: "fancy-charts-hello",
-      name: "Hello",
-      callback: () => new Notice("Fancy Charts: Hello"),
-    });
-
-    // Unit 03: register fancy-charts code block processor here
+    registerPostprocessor(this);
   }
 
-  onunload() {
-    // Unit 03: dispose all active ECharts instances here
-  }
+  onunload() {}
 }
